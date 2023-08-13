1f874ff286f500d98ece4316f649755d5bb463a0
Alexander Ng <alexng353@gmail.com>
2023-08-13 10:46:35 +0900

    tailwind is fun

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="min-h-screen flex flex-col w-full">
+	<div class="bg-black flex-1">Alex</div>
+	<div id="footer" class=" h-16">Footer</div>
+</div>
